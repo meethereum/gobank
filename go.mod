6e0e3f42faad9f5a4a184a33515cac1fae4e7360
meethereum <meethbackup@gmail.com>
2025-01-24 05:29:58 +0530

    create and read done , using postgres

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/meethereum/gobank
+
+go 1.18
+
+require github.com/gorilla/mux v1.8.1
+
+require github.com/lib/pq v1.10.9
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	_"fmt"
+	"log"
+)
+
+func main() {
+	store,err:= NewPostgresStore()
+	if err!=nil{
+		log.Fatal(err)
+	}
+	//fmt.Printf("%+v\n",store)
+	if err:=store.Init(); err!=nil{
+		log.Fatal(err)
+	}
+	server := NewAPIServer(":3000",store)
+	server.Run()
+}

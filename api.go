d2d589ff60dad0373b963529933842fe3b8218b8
meethereum <meethbackup@gmail.com>
2025-01-25 04:20:51 +0530

    added transfer functionality

--- api.go
+++ api.go
@@ -48,7 +48,8 @@ func (s *APIServer) Run() {
 
 	router.HandleFunc("/account", makeHTTPHandleFunc(s.handleAccount))
 	router.HandleFunc("/account/{id}", makeHTTPHandleFunc(s.handleGetAccountById))
-
+	router.HandleFunc("/transfer", makeHTTPHandleFunc(s.handleTransfer))
+	
 	log.Println("JSON API server running on port: ", s.listenAddr)
 
 	http.ListenAndServe(s.listenAddr, router)
@@ -116,5 +117,20 @@ func (s *APIServer) handleDeleteAccount(w http.ResponseWriter, r *http.Request) 
 }
 
 func (s *APIServer) handleTransfer(w http.ResponseWriter, r *http.Request) error {
-	return nil
+	transReq:=new(TransferRequest)
+	if err := json.NewDecoder(r.Body).Decode(transReq); err!=nil{
+		return err
+	}
+	defer r.Body.Close()
+
+	return WriteJSON(w,http.StatusOK,transReq)
+}
+
+func getID(r *http.Request) (int, error) {
+	idStr := mux.Vars(r)["id"]
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		return id, fmt.Errorf("invalid id given %s", idStr)
+	}
+	return id, nil
 }
